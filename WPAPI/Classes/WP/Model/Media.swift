86324fcd2cd27babc53e92de208b68bf9f78402e
Jack Song <jacktator@users.noreply.github.com>
2018-12-20 10:18:41 +1100

    Update Media.swift

--- WPAPI/Classes/WP/Model/Media.swift
+++ WPAPI/Classes/WP/Model/Media.swift
@@ -361,11 +361,29 @@ open class Media : Codable, WPAPI {
 }
 
 public struct MediaDetails : Codable {
+    
     public let width : Int?
     public let height : Int?
     public let file : String?
     public let sizes : Sizes?
     public let imageMeta : ImageMeta?
+    public let title : String?
+    public let filename : String?
+    public let filesize : Int?
+    public let url : String?
+    public let link : String?
+    public let alt : String?
+    public let description : String?
+    public let caption : String?
+    public let name : String?
+    public let status : String?
+    public let uploadedTo : Int?
+    public let date : Date?
+    public let modified : Date?
+    public let menuOrder : Int?
+    public let type : String?
+    public let subtype : String?
+    public let icon : String?
     
     enum CodingKeys: String, CodingKey {
         
@@ -373,16 +391,51 @@ public struct MediaDetails : Codable {
         case height = "height"
         case file = "file"
         case sizes = "sizes"
-        case image_meta = "image_meta"
+        case imageMeta = "image_meta"
+        case title = "title"
+        case filename = "filename"
+        case filesize = "filesize"
+        case url = "url"
+        case link = "link"
+        case alt = "alt"
+        case description = "description"
+        case caption = "caption"
+        case name = "name"
+        case status = "status"
+        case uploadedTo = "uploaded_to"
+        case date = "date"
+        case modified = "modified"
+        case menuOrder = "menu_order"
+        case type = "type"
+        case subtype = "subtype"
+        case icon = "icon"
     }
     
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
+        
         width = try values.decodeIfPresent(Int.self, forKey: .width)
         height = try values.decodeIfPresent(Int.self, forKey: .height)
         file = try values.decodeIfPresent(String.self, forKey: .file)
         sizes = try values.decodeIfPresent(Sizes.self, forKey: .sizes)
-        imageMeta = try values.decodeIfPresent(ImageMeta.self, forKey: .image_meta)
+        imageMeta = try values.decodeIfPresent(ImageMeta.self, forKey: .imageMeta)
+        title = try values.decodeIfPresent(String.self, forKey: .title)
+        filename = try values.decodeIfPresent(String.self, forKey: .filename)
+        filesize = try values.decodeIfPresent(Int.self, forKey: .filesize)
+        url = try values.decodeIfPresent(String.self, forKey: .url)
+        link = try values.decodeIfPresent(String.self, forKey: .link)
+        alt = try values.decodeIfPresent(String.self, forKey: .alt)
+        description = try values.decodeIfPresent(String.self, forKey: .description)
+        caption = try values.decodeIfPresent(String.self, forKey: .caption)
+        name = try values.decodeIfPresent(String.self, forKey: .name)
+        status = try values.decodeIfPresent(String.self, forKey: .status)
+        uploadedTo = try values.decodeIfPresent(Int.self, forKey: .uploadedTo)
+        date = WP.dateFormatter.date(from: try values.decodeIfPresent(String.self, forKey: .date)!)
+        modified = WP.dateFormatter.date(from: try values.decodeIfPresent(String.self, forKey: .modified)!)
+        menuOrder = try values.decodeIfPresent(Int.self, forKey: .menuOrder)
+        type = try values.decodeIfPresent(String.self, forKey: .type)
+        subtype = try values.decodeIfPresent(String.self, forKey: .subtype)
+        icon = try values.decodeIfPresent(String.self, forKey: .icon)
     }
     
     public func encode(to encoder: Encoder) throws {
@@ -393,7 +446,24 @@ public struct MediaDetails : Codable {
         try container.encodeIfPresent(height, forKey: .height)
         try container.encodeIfPresent(file, forKey: .file)
         try container.encodeIfPresent(sizes, forKey: .sizes)
-        try container.encodeIfPresent(imageMeta, forKey: .image_meta)
+        try container.encodeIfPresent(imageMeta, forKey: .imageMeta)
+        try container.encodeIfPresent(title, forKey: .title)
+        try container.encodeIfPresent(filename, forKey: .filename)
+        try container.encodeIfPresent(filesize, forKey: .filesize)
+        try container.encodeIfPresent(url, forKey: .url)
+        try container.encodeIfPresent(link, forKey: .link)
+        try container.encodeIfPresent(alt, forKey: .alt)
+        try container.encodeIfPresent(description, forKey: .description)
+        try container.encodeIfPresent(caption, forKey: .caption)
+        try container.encodeIfPresent(name, forKey: .name)
+        try container.encodeIfPresent(status, forKey: .status)
+        try container.encodeIfPresent(uploadedTo, forKey: .uploadedTo)
+        try container.encodeIfPresent(WP.dateFormatter.string(from: date ?? Date()), forKey: .date)
+        try container.encodeIfPresent(WP.dateFormatter.string(from: modified ?? Date()), forKey: .modified)
+        try container.encodeIfPresent(menuOrder, forKey: .menuOrder)
+        try container.encodeIfPresent(type, forKey: .type)
+        try container.encodeIfPresent(subtype, forKey: .subtype)
+        try container.encodeIfPresent(icon, forKey: .icon)
         
     }
     
